b8bd3bc934d7e3ff8ecc343e6c7669445b65d24c
Nikunj <nikunj.r.rojivadia@gmail.com>
2018-06-10 11:36:23 +0530

    added sql file and updated README

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # url-shortner
 
-Clone the repository onto your local machine.
-To use it locally you must require a websrver and MYSQL database.
-For that you can use <a href="http://www.wampserver.com/">WAMP</a> for Windows, <a href="https://bitnami.com/stack/lamp/installer">LAMP</a> for Linux, <a href="https://www.mamp.info/en/">MAMP</a> for Mac or <a href="https://www.apachefriends.org/index.html">XAMPP</a>.
+<p>Clone the repository onto your local machine.</p>
+<p>To use it locally you must require a websrver and MYSQL database.</p>
+<p>For that you can use <a href="http://www.wampserver.com/">WAMP</a> for Windows, <a href="https://bitnami.com/stack/lamp/installer">LAMP</a> for Linux, <a href="https://www.mamp.info/en/">MAMP</a> for Mac or <a href="https://www.apachefriends.org/index.html">XAMPP</a>.</p>
+<p>Import the url-shortner.sql file to your database</p>
+<p>Enjoy!!!</p>
 
- 
---
+++ url-shortner.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 10, 2018 at 08:00 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `url-shortner`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `links`
+--
+
+CREATE TABLE `links` (
+  `id` int(11) NOT NULL,
+  `url` text NOT NULL,
+  `alias` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `links`
+--
+
+INSERT INTO `links` (`id`, `url`, `alias`) VALUES
+(1, 'https://whatsapp.com', 'kcffH'),
+(2, 'https://youtube.com', 'IXFIc'),
+(3, 'https://whatsapp.com', 'BlPmC'),
+(4, 'https://google.com', 'lXord'),
+(5, 'https://facebook.com', 'nFaAo'),
+(6, 'https://mail.google.com/mail/u/0/h/132w6vnqc07pl/?&', 'JLxVV');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `links`
+--
+ALTER TABLE `links`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `links`
+--
+ALTER TABLE `links`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
